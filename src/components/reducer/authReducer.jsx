c576d74dbcdf0e0979c95525cae71955c374f3a4
Chandan8585 <chandanwingshr@gmail.com>
2023-08-10 23:32:56 +0530

    global state added

---
+++ src/components/reducer/authReducer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const authReducer = (state, {type, payload}) => {
+    switch(type){
+        case "USERNAME":
+            return {
+                ...state,
+                userName: payload
+            }
+        case "PASSWORD":
+            return {
+                ...state,
+                password: payload
+            }
+        case "CLEAR_CREDENTIALS":
+            return{
+                ...state,
+            }
+        default:
+        return state
+        
+    }
+ 
+}
+
+export default authReducer
